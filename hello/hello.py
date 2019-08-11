3dbfe587b5e0819864c841f93f17ea60b523525c
unknown <you@example.com>
2019-08-12 01:22:27 +0800

    testing

--- hello/hello.py
+++ hello/hello.py
@@ -4,4 +4,5 @@ import requests
 
 head_Html_lotto='https://www.taiwanlottery.com.tw/lotto/superlotto638/history.aspx'
 res = requests.get(head_Html_lotto, timeout=30)
+print(res.text)
 print(res.text, file=open('file.txt', 'w'))
