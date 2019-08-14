cb52d0e866ab1b21a180b726fa2a0fcb5b95382c
hli04 <hsin.yi.li@oath.com>
2019-08-14 12:42:21 +0800

    add fund bot

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ def callback():
         abort(400)
     return 'OK'
 
+# movie 
 def movie():
   head_Html_movie='https://movies.yahoo.com.tw/'
   res = requests.get(head_Html_movie, timeout=30)
@@ -45,6 +46,26 @@ def movie():
         content += '{}\n{}\n'.format(title, link)
   return content
 
+# 基金
+def fund1():
+ head_Html_lotto='https://tw.money.yahoo.com/fund/history/F0HKG05X22:FO'
+ res = requests.get(head_Html_lotto, timeout=30)
+ soup = BeautifulSoup(res.text, 'html.parser')
+ content = []
+ content1 = []
+ finalResult = ""
+ for index, data in enumerate(soup.select('div#recent-price')): 
+   for index, date in  enumerate(data.select('td.short-date')) :
+     date = date.text
+     content.append(date)
+     for index, value in  enumerate(data.select('td.zero')) :
+      value = value.text
+      content1.append(value)
+ hash = {k:v for k, v in zip(content, content1)}
+
+ for key, value in hash.items():
+    finalResult = "日期: {}, 淨值:{}".format(key,value)
+    return finalResult
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -56,6 +77,9 @@ def handle_message(event):
         #Your user ID
     elif(text=="最新電影"):
         a=movie()
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=a))
+    elif(text=="安聯台灣科技"):
+        a=fund1()
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=a))
     elif(text=="你好"):
         reply_text = "哈囉"
--- hello/hello.py
+++
@@ -1,8 +0,0 @@
-
-from bs4 import BeautifulSoup
-import requests
-
-head_Html_lotto='https://www.taiwanlottery.com.tw/lotto/superlotto638/history.aspx'
-res = requests.get(head_Html_lotto, timeout=30)
-print(res.text)
-print(res.text, file=open('file.txt', 'w'))
