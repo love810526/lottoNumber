af2ad4aa2e21ef683afb86046a21f81e023591d5
unknown <you@example.com>
2021-02-07 18:53:10 +0800

    fix gold

--- money/gold.py
+++ money/gold.py
@@ -9,12 +9,12 @@ date = ""
 sell = ""
 sold = ""
 for index, data in enumerate(soup.select('div.cf div')):
-    date = data.text  
+    date = data.text.strip()  
 
 for index, detail in enumerate(soup.select('div.hasscript-div td.text-right')):
     if index < 1:
-        sell = detail.text
+        sell = detail.text.strip()
     if index < 2:
-        sold = detail.text
-content += "{}\n 本行賣出:{}\n 本行買進:{}\n".format(date, sell, sold)
+        sold = detail.text.strip()
+content += "{}\n本行賣出:{}\n本行買進:{}\n".format(date, sell, sold)
 print(content)
